a472f7e7af228eda0b0405f1301419106a7ed260
Sagiri777 <guoziming08@126.com>
2026-02-28 19:38:12 +0800

    feat:测试

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.core.utils.astrbot_path import get_astrbot_data_path
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("helpmenu", "Sagiri777", "一个简单的 HelpMenu 插件", "0.0.1")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -10,14 +11,16 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    # 注册指令的装饰器。指令名为 helpmenu。注册成功后，发送 `/helpmenu` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("helpmenu")
+    async def helpmenu(self, event: AstrMessageEvent):
+        """这是一个帮助菜单指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
+        message_str = event.message_str
+        plugins = self.context.get_all_stars()
+        plugin_data_path = get_astrbot_data_path()
+        with open(f"{plugin_data_path}/example.txt", "w") as f:
+            f.write(plugins)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
     async def terminate(self):
